328119c7501a29d123de433760ad0140503b90d9
Aishiteru <thanhuy97@gmail.com>
2023-01-25 21:22:11 +0700

    init project

---
+++ backend/src/main/java/com/fu/swp/childcare/ChildcareApplication.java
@@ -0,0 +1,13 @@
+package com.fu.swp.childcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildcareApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/fu/swp/childcare/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.fu.swp.childcare.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String testConnection(){
+        return "hello world";
+    }
+}
