9529bb2658fada5437de7be08c7f3a53d420c8ef
thanhnh <thanhuy97@gmail.com>
2023-02-08 15:10:16 +0700

    commit

--- backend/src/main/java/com/fu/swp/childcare/security/WebSecurityConfig.java
+++ backend/src/main/java/com/fu/swp/childcare/security/WebSecurityConfig.java
@@ -34,38 +34,29 @@ public class WebSecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        requestHandler.setCsrfRequestAttributeName("_csrf");
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .cors().configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setExposedHeaders(Arrays.asList("Authorization"));
-                        config.setMaxAge(3600L);
-                        return config;
-                    }
-                }).and().csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact","/register")
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
-                .addFilterAt(new AuthoritiesLoggingAtFilter(),BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
-                .authorizeHttpRequests()
-                .requestMatchers("/myAccount").hasRole("USER")
-                .requestMatchers("/admin").hasAnyRole("ADMIN")
-                .requestMatchers("/manager").hasRole("MANAGER")
-                .requestMatchers("/staff").hasRole("STAFF")
-                .requestMatchers("/user").authenticated()
-                .requestMatchers("/","/signin","/register").permitAll()
-                .and().formLogin()
-                .and().httpBasic();
+        http.cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests ()
+                .requestMatchers ("/", "/api/auth/signup").permitAll()
+                .requestMatchers ("/api/auth/signin").permitAll()
+                .requestMatchers ("/api/test/**").permitAll()
+                .requestMatchers ("/admin/**").hasAnyAuthority("ADMIN")
+                .requestMatchers ("/account/**").hasAnyAuthority("USER")
+                .requestMatchers("/manage/**").hasAnyAuthority("MANAGER")
+                .requestMatchers("/staff/**").hasAnyAuthority("STAFF")
+                .anyRequest().authenticated()
+                .and()
+                // logout
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied");
+//                .requestMatchers ("localhost/8080" + "/**").permitAll()
+        // fix H2 database console: Refused to display ' in a frame because it set 'X-Frame-Options' to 'deny'
+        http.headers().frameOptions().sameOrigin();
+
         return http.build();
     }
 
@@ -73,7 +64,6 @@ public class WebSecurityConfig {
     public UserDetailsService userDetailsService(DataSource dataSource){
         return new JdbcUserDetailsManager(dataSource);
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder(){
