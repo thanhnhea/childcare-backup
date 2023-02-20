3e1fd4123298f922fc69a3ce760f25c9b31bb71f
DUCPQ <ducpq1>
2023-02-21 03:21:04 +0700

    add role into register user

--- backend/src/main/java/com/fu/swp/childcare/controller/user/UserController.java
+++ backend/src/main/java/com/fu/swp/childcare/controller/user/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/account")
 public class UserController {
