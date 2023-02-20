2d0639e407515c2f783511883dd474caec9286cc
thanhnh <thanhuy97@gmail.com>
2023-02-21 02:03:50 +0700

    authen , authorize frontend.

--- frontend/src/Pages/Home/Header/Header.jsx
+++ frontend/src/Pages/Home/Header/Header.jsx
@@ -1,38 +1,95 @@
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faUser } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../../Images/logo.png';
 import './Header.css';
 
-const Header = () => {
+import AuthService from "../../../services/auth.service";
+import { Component } from 'react';
 
-    return (
-        <div className="head-bg">
-            <Navbar className="navbar" collapseOnSelect expand="lg">
-                <Container className="container-head">
-                    <Navbar.Brand href="/home"><img src={logo} alt="logo" /></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" expand="lg"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto align-items-center">
-                            <Link to="/home" className='list-item text-decoration-none'>Home</Link>
-                            <Link to="/about" className='list-item text-decoration-none'>About</Link>
-                            <Link to="/service" className='list-item text-decoration-none'>Service</Link>
-                            <Link to="/doctor" className='list-item text-decoration-none'>Doctor</Link>
-                            <Link to="/contact" className='list-item text-decoration-none'>Contact</Link>
-                          
-                            <Link to="/login" type="button" className="btn btn-danger">Login</Link>
-                         
-                           
-                                <Navbar.Text><a href="/UserDetails"><FontAwesomeIcon icon={faUser} /><span className="userName"></span></a></Navbar.Text>
-                       
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    );
-};
+class Header extends Component {
 
+    constructor(props) {
+        super(props);
+        this.logOut = this.logOut.bind(this);
+
+        this.state = {
+            showModeratorBoard: false,
+            showAdminBoard: false,
+            currentUser: undefined,
+        };
+    }
+
+    componentDidMount() {
+        const user = AuthService.getCurrentUser();
+
+        if (user) {
+            this.setState({
+                currentUser: user,
+                showModeratorBoard: user.roles.includes("ROLE_MANAGER"),
+                showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+            });
+        }
+    }
+
+    logOut() {
+        AuthService.logout();
+        this.setState({
+            showModeratorBoard: false,
+            showAdminBoard: false,
+            currentUser: undefined,
+        });
+    }
+    render() {
+        const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+        return (
+            <div className="head-bg">
+                <Navbar className="navbar" collapseOnSelect expand="lg">
+                    <Container className="container-head">
+                        <Navbar.Brand href="/home"><img src={logo} alt="logo" /></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" expand="lg" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ms-auto align-items-center">
+                                <Link to="/home" className='list-item text-decoration-none'>Home</Link>
+                                <Link to="/about" className='list-item text-decoration-none'>About</Link>
+                                <Link to="/service" className='list-item text-decoration-none'>Service</Link>
+                                <Link to="/doctor" className='list-item text-decoration-none'>Doctor</Link>
+                                <Link to="/contact" className='list-item text-decoration-none'>Contact</Link>
+                                {showModeratorBoard && (
+                                    <Link to={"/mod"} className="list-item text-decoration-none">
+                                        Manager Board
+                                    </Link>
+                                )}
+                                {showAdminBoard && (
+                                    <Link to={"/admin"} className="list-item text-decoration-none">
+                                        Admin Board
+                                    </Link>
+                                )}
+
+                                {currentUser ? (
+                                    <div className="navbar-nav ml-auto">
+                                        <li className="nav-item">
+                                            <Link to={"/profile"} className="list-item text-decoration-none">
+                                                {currentUser.username}
+                                            </Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a href="/login" className="list-item text-decoration-none" onClick={this.logOut}>
+                                                LogOut
+                                            </a>
+                                        </li>
+                                    </div>
+                                ) : (<Link to="/login" type="button" className="btn btn-danger">Login</Link>)}
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+// <Navbar.Text><a href="/UserDetails"><FontAwesomeIcon icon={faUser} /><span className="userName"></span></a></Navbar.Text>
 export default Header;
---
+++ frontend/src/Pages/Login/Login.js
@@ -0,0 +1,162 @@
+import { faFacebook, faGithub, faGoogle, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Component } from 'react';
+import serviceDetailImg from '../../Images/service-details-promo1.png';
+import './Login.css';
+
+
+import AuthService from "../../services/auth.service";
+import { withRouter } from '../../common/with-router';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+
+        this.state = {
+            username: "",
+            password: "",
+            loading: false,
+            message: ""
+        };
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        });
+    }
+
+    handleLogin(e) {
+        e.preventDefault();
+
+        this.setState({
+            message: "",
+            loading: true
+        });
+        AuthService.login(this.state.username, this.state.password).then(
+            () => {
+                this.props.router.navigate("/profile");
+                window.location.reload();
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+
+                this.setState({
+                    loading: false,
+                    message: resMessage
+                });
+            }
+        );
+    }
+    render() {
+        return (
+            <div>
+                <section className="h-100 gradient-form pb-5">
+                    <div className="container py-5 h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col-xl-10">
+                                <div className="card rounded-3 text-black">
+                                    <div className="row g-0">
+                                        <div className="col-lg-6">
+                                            <div className="card-body p-md-5 mx-md-4">
+                                                <div className="text-center">
+                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp" className="card-img" alt="logo" />
+                                                    <h4 className="mt-1 mb-5 pb-1">We are The Children Care Team</h4>
+                                                </div>
+                                                <p className="d-flex justify-content-start">Please login to your account</p>
+                                                <form
+                                                    onSubmit={this.handleLogin}
+                                                    ref={c => {
+                                                        this.form = c;
+                                                    }}
+                                                >
+                                                    <div className="form-outline mb-4">
+                                                        <label htmlFor="username">Username</label>
+                                                        <input
+                                                            type="text" className="form-control"
+                                                            name="username"
+                                                            value={this.state.username}
+                                                            onChange={this.onChangeUsername}
+                                                            required pattern="[a-zA-Z0-9]+" />
+                                                    </div>
+
+                                                    <div className="form-outline mb-4">
+                                                        <label htmlFor="password">Password</label>
+                                                        <input
+                                                            type="password"
+                                                            value={this.state.password}
+                                                            onChange={this.onChangePassword}
+                                                            className="form-control"
+                                                            required minLength="8" />
+                                                    </div>
+
+                                                    <div className="text-center pt-1 mb-5 pb-1">
+                                                        <div type="submit" className="theme-btn btn-fill" onClick={this.handleLogin}>
+                                                            <span>Login</span>
+                                                        </div>
+                                                        <a className="text-muted text-decoration-none" href="ChangePasword">Forgot password?</a>
+                                                    </div>
+
+                                                    <div className="d-flex align-items-center justify-content-center pb-4">
+                                                        <p className="mb-0 me-2">Don't have an account?</p>
+                                                        <button type="button"
+                                                            className="btn btn-outline-danger">
+                                                            <a style={{ textDecoration: 'None' }} href="/register">Create new</a>
+                                                        </button>
+                                                    </div>
+
+                                                </form>
+
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                                                <h4 className="mb-4">We are more than just a company</h4>
+                                                <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                                <img src={serviceDetailImg} alt="expertDoctor" className="img-fluid pt-xs-5" />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row my-5">
+                                        <div className="col-12">
+                                            <div className="text-center">
+                                                <h4>or sign up with:</h4>
+                                                <div className="doctors-social">
+                                                    <button className="loginbtn" ><FontAwesomeIcon icon={faGoogle} /></button>
+                                                    <button className="loginbtn" ><FontAwesomeIcon icon={faGithub} /></button>
+                                                    <button className="loginbtn" ><FontAwesomeIcon icon={faFacebook} /></button>
+                                                    <button className="loginbtn"><FontAwesomeIcon icon={faTwitter} /></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+
+};
+
+
+export default withRouter(Login); 
