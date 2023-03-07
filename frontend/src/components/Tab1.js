1f260183c43cb8bb84ba68e94ec44fddf77f27e0
thanhnh <thanhuy97@gmail.com>
2023-03-07 17:02:55 +0700

    commit

--- frontend/src/components/Tab1.js
+++ frontend/src/components/Tab1.js
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import userService from "../services/user.service";
 
 const Tab1 = () => {
 
     const [selectedChildren, setSelectedChildren] = useState([]);
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await userService.getModeratorBoard();
+            setData(response.data);
+        }
+        fetchData();
+    }, []);
+
+    console.log(data);
 
     const items = [
         { firstName: 'John', lastName: 'Doe', dob: '01/01/1990', status: 'Assigned' },
@@ -11,12 +24,12 @@ const Tab1 = () => {
         { firstName: 'Alice', lastName: 'Johnson', dob: '04/04/1992', status: 'Inactive' },
     ];
 
-    const sortedItems = items.sort((b, a) => a.status.localeCompare(b.status));
+    // const sortedItems = data.sort((b, a) => a.status.localeCompare(b.status));
 
     const handleCheckboxChange = (event, index) => {
         const isChecked = event.target.checked;
-        const itemStatus = sortedItems[index].status;
-        if (itemStatus === 'Inactive') {
+        const itemStatus = data[index].status;
+        if (itemStatus === 0) {
             if (isChecked) {
                 setSelectedChildren([...selectedChildren, index]);
             } else {
@@ -34,7 +47,7 @@ const Tab1 = () => {
         <div>
             <h3>Tab 1</h3>
             <ul className="list-group">
-                {sortedItems.map((item, index) => (
+                {data.map((item, index) => (
                     <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
                         <div className="form-check">
                             <input
@@ -43,7 +56,7 @@ const Tab1 = () => {
                                 id={`checkbox-${index}`}
                                 checked={selectedChildren.includes(index)}
                                 onChange={(event) => handleCheckboxChange(event, index)}
-                                disabled={item.status !== 'Inactive'}
+                                disabled={item.status !== 0}
                             />
                             <label className="form-check-label" htmlFor={`checkbox-${index}`}>
                                 {item.firstName} {item.lastName} ({item.dob}) - {item.status}
